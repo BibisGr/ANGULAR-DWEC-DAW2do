5d42bbffa3be8952cfe185d7ba03677e90810b47
Bibis Gr <bertha.garrastacho@digitechfp.com>
2025-10-26 19:43:08 +0100

    actualizacion

--- Clase-8/Clase-8/src/app/app.routes.ts
+++ Clase-8/Clase-8/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './componentes/product-list/product-list';
+import { ProductDetails } from './componentes/product-details/product-details';
+import { ProductCard } from './componentes/product-card/product-card';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',redirectTo: '/product-list', pathMatch: 'full'},
+  {path: 'product-list', component: ProductList},
+  {path: 'product/details/:id', component: ProductDetails},
+  {path: 'product-card', component: ProductCard},
+
+];
