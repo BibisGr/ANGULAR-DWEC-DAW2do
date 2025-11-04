f610b182b761bb062760ea968ce8cc8661c087a8
Bibis Gr <bertha.garrastacho@digitechfp.com>
2025-11-04 19:53:33 +0100

    prep clase 10

---
+++ Clase-10/src/app/componentes/pokemon-cards/pokemon-cards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-cards',
+  imports: [],
+  templateUrl: './pokemon-cards.html',
+  styleUrl: './pokemon-cards.css',
+})
+export class PokemonCards {
+
+}
---
+++ Clase-10/src/app/componentes/weapon-valorant/weapon-valorant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weapon-valorant',
+  imports: [],
+  templateUrl: './weapon-valorant.html',
+  styleUrl: './weapon-valorant.css',
+})
+export class WeaponValorant {
+
+}
